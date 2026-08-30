7c26473aa4cd7fc2cc9910b91c757e145f1c33f1
ahaxhiu3918 <arionhaxhiu@gmail.com>
2026-08-30 17:49:23 +0200

    edge-to-edge testing

--- sync.py
+++ sync.py
@@ -17,7 +17,7 @@ def hash_file(path):
             buf = file.read(BLOCKSIZE)
 
     return hasher.hexdigest()
-
+""" 
 def sync(source, dest):
     # Walk the source folder and build a dict of filenames and their hashes
     source_hashes = {}
@@ -44,7 +44,47 @@ def sync(source, dest):
     for src_hash, fn in source_hashes.items():
         if src_hash not in seen:
             shutil.copy(Path(source) / fn, Path(dest) / fn)
+"""
 
+source_files = {'hash1': 'path1', 'hash2': 'path2'}
+dest_files = {'hash1': 'path1', 'hash2': 'pathX'}
+
+
+def sync(source, dest):
+    # imperative shell step 1, gather inputs
+    source_hashes = read_paths_and_hashes(source)
+    dest_hashes = read_paths_and_hashes(dest)
+    # step 2: call functional core
+    actions = determine_actions(source_hashes, dest_hashes, source, dest)
+    # imperative shell step 3, apply outputs
+    for action, *paths in actions:
+        if action == 'copy':
+            shutil.copyfile(*paths)
+        if action == 'move':
+            shutil.move(*paths)
+        if action == 'delete':
+            os.remove(paths[0])
+
+def read_paths_and_hashes(root):
+    hashes = {}
+    for folder, _, files in os.walk(root):
+        for fn in files:
+            hashes[hash_file(Path(folder) / fn)] = fn
+    return hashes   
+
+def determine_actions(src_hashes, dst_hashes, src_folder, dst_folder):
+    for sha, filename in src_hashes.items():
+        if sha not in dst_hashes:
+            sourcepath = Path(src_folder) / filename
+            destpath = Path(dst_folder) / filename
+            yield 'copy', sourcepath, destpath
+        elif dst_hashes[sha] != filename:
+            olddestpath = Path(dst_folder) / dst_hashes[sha]
+            newdestpath = Path(dst_folder) / filename
+            yield 'move', olddestpath, newdestpath
+    for sha, filename in dst_hashes.items():
+        if sha not in src_hashes:
+            yield 'delete', dst_folder / filename     
 
 
             
